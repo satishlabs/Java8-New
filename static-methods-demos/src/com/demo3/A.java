00cf9b3ece3c159c06edd78f374f978c9c25aee3
Satish Prasad <spd61185@gmail.com>
2021-08-20 00:47:23 +0530

    static-methods-demos

--- static-methods-demos/src/com/demo3/A.java
+++ static-methods-demos/src/com/demo3/A.java
@@ -6,5 +6,13 @@ public interface A {
 		System.out.println("A -> m1()");
 	}
 
-	
+	default void m2() {
+		System.out.println("A -> m2()");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("main method");
+		m1();
+		//m2();
+	}
 }
