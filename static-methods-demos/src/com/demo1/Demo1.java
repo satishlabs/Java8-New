35163ebfd6c7acae505eb30613d49f230d3f10e3
Satish Prasad <spd61185@gmail.com>
2021-08-20 00:23:29 +0530

    static-methods-demos

--- static-methods-demos/src/com/demo1/Demo1.java
+++ static-methods-demos/src/com/demo1/Demo1.java
@@ -2,6 +2,7 @@ package com.demo1;
 
 public class Demo1 {
 	public static void main(String[] args) {
-		
+		Hello hello = new Hello();
+		hello.test();
 	}
 }
--- static-methods-demos/src/com/demo1/Hello.java
+++ static-methods-demos/src/com/demo1/Hello.java
@@ -2,6 +2,21 @@ package com.demo1;
 
 public class Hello implements A {
 
+	
+	
+	public void test() {
+		System.out.println(P); //Inherited
+		System.out.println(Q); //Inherited
+		m1(); //Overriden
+		m2(); //Overriden
+		m3(); //Overriden
+		A.super.m3();
+		m4(); //Inherited
+		A.super.m4();
+		A.m5();
+		A.m6();
+		//A.super.m6();
+	}
 	@Override
 	public void m1() {
 		System.out.println("Hello -> m1()");
@@ -12,15 +27,11 @@ public class Hello implements A {
 		System.out.println("Hello -> m2()");
 	}
 	
-	public void test() {
-		System.out.println(P);
-		System.out.println(Q);
-		m1();
-		m2();
-		m3();
-		m4();
-		m5();
-		m6();
+	@Override
+	public void m3() {
+		System.out.println("Hello -> m3()");
 	}
-
+	/*
+	 * @Override public void m5() { System.out.println("Hello -> m5()"); }
+	 */
 }
