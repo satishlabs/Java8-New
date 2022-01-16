1f04a64379e662b241412450abe97085d20a338f
Satish Prasad <spd61185@gmail.com>
2022-01-16 16:16:25 +0530

    Added functioanl interface - BiPredicate with conditions

---
+++ functional-interfaces-demos/src/com/satishlabs/demo3/Demo8.java
@@ -0,0 +1,17 @@
+package com.satishlabs.demo3;
+
+import java.util.function.BiPredicate;
+
+public class Demo8 {
+	public static void main(String[] args) {
+
+		BiPredicate<Integer,Integer> predicate1 = (num1,num2)->num1>num2;
+
+		boolean mybool = predicate1.test(13, 21);
+		System.out.println(mybool);
+
+		BiPredicate<Integer,Integer> predicate2 = (num1,num2)->num1<num2;
+		mybool = predicate2.test(13, 21);
+		System.out.println(mybool);
+	}
+}
