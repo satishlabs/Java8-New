c2b30106c99d4a59bd0a050834ec4cbcbf58b064
Satish Prasad <spd61185@gmail.com>
2022-01-22 12:43:29 +0530

    Optional with Optinal.of() method with null

---
+++ optinal-demos/src/com/demo2/Demo3.java
@@ -0,0 +1,13 @@
+package com.demo2;
+
+import java.util.Optional;
+
+public class Demo3 {
+	public static void main(String[] args) {
+		
+		//Optional.of() with null
+		String str=null;
+		Optional<String> myopts = Optional.of(str);
+		System.out.println(myopts);
+	}
+}
