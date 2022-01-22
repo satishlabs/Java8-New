f719f5398d08bc6f5ffb64b8b082e63ab303e6fe
Satish Prasad <spd61185@gmail.com>
2022-01-22 12:55:14 +0530

    Optional with ofNullable() method

---
+++ optinal-demos/src/com/demo2/Demo5.java
@@ -0,0 +1,21 @@
+package com.demo2;
+
+import java.util.Optional;
+
+public class Demo5 {
+	public static void main(String[] args) {
+		//Optional with ofNullable() method
+		String str= "Satish Prasad";
+		Optional<String> myopts = Optional.ofNullable(str);
+		System.out.println("1. "+myopts);
+		System.out.println("2. "+myopts.orElse("Hello Guys"));
+		System.out.println("3. "+myopts);
+		System.out.println("4. "+myopts.isPresent());
+		
+		if(myopts.isPresent()) {
+			System.out.println("5. "+myopts.get());
+		}else {
+			System.out.println("6. No Value Present");
+		}
+	}
+}
