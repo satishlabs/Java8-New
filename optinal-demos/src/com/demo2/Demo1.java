6b22ecb99144414e18ddc049c6701b1a52bee44d
Satish Prasad <spd61185@gmail.com>
2022-01-22 12:30:07 +0530

    Optional with Optinal.empty() method

---
+++ optinal-demos/src/com/demo2/Demo1.java
@@ -0,0 +1,23 @@
+package com.demo2;
+
+import java.util.Optional;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		//Optiona are private constructos, direct object creational is not possible
+		//Optional<String> myopts = new Optional<String>();
+		
+		Optional<String> myopts = Optional.empty();
+		System.out.println("1. "+myopts);
+		System.out.println("2. "+myopts.orElse("Hello Guys"));
+		System.out.println("3. "+myopts);
+		System.out.println("4. "+myopts.isPresent());
+		//System.out.println("5. "+myopts.get()); //Exception in thread "main" java.util.NoSuchElementException: No value present
+		
+		if(myopts.isPresent()) {
+			System.out.println("5. "+myopts.get());
+		}else {
+			System.out.println("No value found");
+		}
+	}
+}
