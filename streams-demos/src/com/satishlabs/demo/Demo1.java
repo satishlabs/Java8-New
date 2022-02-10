90dd5f1289908ffdec9a878badec286e12818fb2
Satish Prasad <spd61185@gmail.com>
2022-02-10 11:52:19 +0530

    Stream exmaples

--- streams-demos/src/com/satishlabs/demo/Demo1.java
+++ streams-demos/src/com/satishlabs/demo/Demo1.java
@@ -1,5 +1,22 @@
 package com.satishlabs.demo;
 
-public class Demo1 {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
+public class Demo1 {
+	public static void main(String[] args) {
+		List<Integer> numList = Arrays.asList(1,2,3,4,5,6,7,8,9);
+		
+		//Created Stream
+		Stream<Integer> mystream = numList.stream(); //1
+		
+		//Applyed Intermediate Operation
+		Stream<Integer> oddStream = mystream.filter(num -> num%2 != 0); //2
+		Stream<Integer> squerStream = oddStream.map(num -> num*num);//2
+		
+		squerStream.forEach(System.out::println); //3
+		
+		
+	}
 }
