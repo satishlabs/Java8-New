c2390c58fa0d2fb84096260e6e16c918e1021041
Satish Prasad <spd61185@gmail.com>
2022-02-21 10:22:36 +0530

    Stream examples

---
+++ streams-demos/src/com/satishlabs/demo/Demo8.java
@@ -0,0 +1,16 @@
+package com.satishlabs.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Demo8 {
+	public static void main(String[] args) {
+		List<Integer> myList = Arrays.asList(5,2,7,8,3,9,9,4,2,3,7,1,6,1);
+		myList.stream()
+			.limit(15)
+			.filter(num -> num%2!=0)
+			.map(num -> num*num)
+			.sorted()
+			.forEach(System.out::println);
+	}
+}
